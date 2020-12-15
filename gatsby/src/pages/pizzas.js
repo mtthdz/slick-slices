2285203f393d6881f182f3931cb33d818238f329
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-12-15 15:41:35 -0500

    created page query for pizzas

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-export default function PizzaPage() {
+export default function PizzasPage({ data }) {
+  const pizzas = data.pizzas.nodes;
   return (
     <>
-      <p>Hey! I'm the Pizza Page!</p>
+      <p>Hey! There are {pizzas.length} pizzas!</p>
     </>
   );
 }
+// page query
+// gatsby will know that you're calling graphql here and run it in and store in the props param
+// note the query looks like how its written in graphiql
+export const query = graphql`
+  query PizzaQuery {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
