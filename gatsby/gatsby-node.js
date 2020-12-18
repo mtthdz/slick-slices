656f11ee7b48a34761101bcdbf13dd6c85d6c0c1
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-12-18 12:29:10 -0500

    created gatsby node file to start creating dynamic pages

---
+++ gatsby/gatsby-node.js
@@ -0,0 +1,12 @@
+async function turnPizzasIntoPages(params) {
+  // 1. get a template for this page
+  // 2. query all pizzas
+  // 3. loop over each pizza and create a page for that pizza
+}
+
+export async function createPages(params) {
+  // create pages dynamically
+  // 1. pizzas
+  // 2. toppings
+  // 3. slicemasters
+}
