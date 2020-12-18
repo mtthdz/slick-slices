df9f3e032f8ed997befae40628f5aed18a5edaa2
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-12-18 12:03:15 -0500

    wrote functions to pull toppings and pizzas data and sort

--- gatsby/src/components/ToppingsFilter.js
+++ gatsby/src/components/ToppingsFilter.js
@@ -1,8 +1,60 @@
+import { graphql, useStaticQuery } from 'gatsby';
 import React from 'react';
 
+function countPizzasInToppings(pizzas) {
+  // return the pizzas with counts
+  // flat will make an array of arrays
+  const counts = pizzas
+    .map((pizza) => pizza.toppings)
+    .flat()
+    .reduce((acc, topping) => {
+      // check if this is an existing topping
+      // if it is, increment; else, create new entry in acc and set to 1
+      const existingTopping = acc[topping.id];
+      if (existingTopping) {
+        existingTopping.count += 1;
+      }
+      acc[topping.id] = {
+        id: topping.id,
+        name: topping.name,
+        count: 1,
+      };
+      return acc;
+    }, {});
+
+  // sort toppings based on their count
+  const sortedToppings = Object.values(counts).sort(
+    (a, b) => b.count - a.count
+  );
+  return sortedToppings;
+}
+
 export default function ToppingsFilter() {
-  // get a list of all toppings
-  // get a list of all pizzas with their toppings
+  // 1. get a list of all toppings
+  // 2. get a list of all pizzas with their toppings
+  const { toppings, pizzas } = useStaticQuery(graphql`
+    query {
+      toppings: allSanityTopping {
+        nodes {
+          name
+          id
+          vegetarian
+        }
+      }
+      pizzas: allSanityPizza {
+        nodes {
+          toppings {
+            name
+            id
+          }
+        }
+      }
+    }
+  `);
+
+  // countPizzasInToppings fn call
+  const toppingsWithCounts = countPizzasInToppings(pizzas.nodes);
+  console.log(toppingsWithCounts);
   // count how many pizzas are in each toppings
   // loop over list of toppings to display toppings and amount of pizzas for each
 
