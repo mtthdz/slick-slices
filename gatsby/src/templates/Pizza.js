f5f997918694c97bee7de2736bbe03c4b0cd7926
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-12-28 18:00:15 -0500

    completed template for individual pizza page

--- gatsby/src/templates/Pizza.js
+++ gatsby/src/templates/Pizza.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
 
-export default function SinglePizzaPage() {
-  return <p>Nice</p>;
+const PizzaGrid = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+`;
+
+// how to destructure a property two levels deep in a parameter:
+export default function SinglePizzaPage({ data: { pizza } }) {
+  return (
+    <PizzaGrid>
+      <Img fluid={pizza.image.asset.fluid} />
+      <div>
+        <h2 className="mark">{pizza.name}</h2>
+        <ul>
+          {pizza.toppings.map((topping) => (
+            <li key={topping.id}>{topping.name}</li>
+          ))}
+        </ul>
+      </div>
+    </PizzaGrid>
+  );
 }
 
 // this needs to be dynamic based on slug passed via context in gatsby-node
