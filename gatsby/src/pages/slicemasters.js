1b430a41cd02befc58fbd56ade246e1d0fb1eff6
Matthew Hernandez <matt.hernandez94@gmail.com>
2021-01-04 16:17:27 -0500

    completed markup of slicemasters page

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
+import Img from 'gatsby-image';
 
 export default function SlicemastersPage({ data }) {
   const slicemasters = data.slicemasters.nodes;
-  console.log(slicemasters);
+
   return (
     <>
-      <p>Hey! I'm the Slicemasters Page!</p>
+      <div>
+        {slicemasters.map((person) => (
+          <div key={person.id}>
+            <Link to={`/slicemaster/${person.slug.current}`}>
+              <h2>
+                <span className="mark">{person.name}</span>
+              </h2>
+            </Link>
+            <Img fluid={person.image.asset.fluid} />
+            <p className="description">{person.description}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
