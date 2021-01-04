cce6d33a8f79224b9838038e88fa9fd092fd5581
Matthew Hernandez <matt.hernandez94@gmail.com>
2021-01-04 17:03:17 -0500

    added base pagination logic to slicemasters page

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -84,11 +84,40 @@ async function fetchBeersAndTurnIntoNodes({
   }
 }
 
-async function turnSlicemastersIntoPages({ graphql, actions}) {
+async function turnSlicemastersIntoPages({ graphql, actions }) {
   // 1. query all slicemasters
+  const { data } = await graphql(`
+    query {
+      slicemasters: allSanityPerson {
+        totalCount
+        nodes {
+          name
+          id
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+
   // TODO: 2. turn each slicemaster into their own page
   // 3. figure out how many pages there are based on how many slicemasters per page
+  const pageSize = parseInt(process.env.GATSBY_PAGE_SIZE);
+  const pageCount = Math.ceil(data.slicemasters.totalCount / pageSize);
   // 4. loop from 1 to n and create pages for them
+  Array.from({ length: pageCount }).forEach((_, i) => {
+    actions.createPage({
+      path: `/slicemasters/${i + 1}`,
+      component: path.resolve('./src/pages/slicemasters.js'),
+      // this data is passed to the template when we create it
+      context: {
+        skip: i * pageSize,
+        currentPage: i + 1,
+        pageSize,
+      },
+    });
+  });
 }
 
 export async function sourceNodes(params) {
