9707e970d579bb7ca43037c61d136d54a0568766
Matthew Hernandez <matt.hernandez94@gmail.com>
2021-01-04 18:08:59 -0500

    added navigation logic to pagination component, fixed pageSize prop origin

--- gatsby/src/components/Pagination.js
+++ gatsby/src/components/Pagination.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import { Link } from 'gatsby';
+import styled from 'styled-components';
 
-export default function Pagination() {
-  return <div>
-    <p>nice</p>
-  </div>
+export default function Pagination({
+  pageSize,
+  totalCount,
+  currentPage,
+  skip,
+  base,
+}) {
+  const totalPages = Math.ceil(totalCount / pageSize);
+  const prevPage = currentPage - 1;
+  const nextPage = currentPage + 1;
+  const hasNextPage = nextPage <= totalPages;
+  const hasPrevPage = prevPage >= 1;
+  return (
+    <div>
+      <Link disabled={!hasPrevPage} to={`${base}/${prevPage}`}>
+        ← Prev
+      </Link>
+      {Array.from({ length: totalPages }).map((_, i) => (
+        <Link to={`${base}/${i + 1}`}>{i + 1}</Link>
+      ))}
+      <Link disabled={!hasNextPage} to={`${base}/${nextPage}`}>
+        Next →
+      </Link>
+    </div>
+  );
 }
