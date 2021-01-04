5fce62693a8d728e37d38f17a2ad44bfc73bad1d
Matthew Hernandez <matt.hernandez94@gmail.com>
2021-01-04 16:12:29 -0500

    added query to slicemasters page

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-export default function SlicemastersPage() {
+export default function SlicemastersPage({ data }) {
+  const slicemasters = data.slicemasters.nodes;
+  console.log(slicemasters);
   return (
     <>
       <p>Hey! I'm the Slicemasters Page!</p>
     </>
   );
 }
+
+export const query = graphql`
+  query {
+    slicemasters: allSanityPerson {
+      totalCount
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        description
+        image {
+          asset {
+            fluid(maxWidth: 410) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
