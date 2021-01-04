0154b7a9dcd1366419b1575a45fa4e3b2d9014c4
Matthew Hernandez <matt.hernandez94@gmail.com>
2021-01-04 14:39:02 -0500

    updated beers page with query and template, updated external beer api link

--- gatsby/src/pages/beers.js
+++ gatsby/src/pages/beers.js
@@ -1,9 +1,41 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-export default function BeersPage() {
+export default function BeersPage({ data }) {
   return (
     <>
-      <p>Hey! I'm the Beers Page!</p>
+      <h2 className="center">
+        We have ${data.beers.nodes.length} Beers Available. Dine in Only!
+      </h2>
+      <div>
+        {data.beers.nodes.map((beer) => {
+          console.log(beer);
+          return (
+            <div key={beer.id}>
+              <img src={beer.image} alt={beer.name} />
+              <h3>{beer.name}</h3>
+              {beer.price}
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
+
+export const query = graphql`
+  query {
+    beers: allBeer {
+      nodes {
+        id
+        name
+        price
+        image
+        rating {
+          average
+          reviews
+        }
+      }
+    }
+  }
+`;
