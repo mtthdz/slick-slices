4fa96a4e43f9891c9494d388f976c156b7668fb4
Matthew Hernandez <matt.hernandez94@gmail.com>
2021-01-14 16:53:42 -0500

    added query for individual slicemaster

--- gatsby/src/templates/Slicemaster.js
+++ gatsby/src/templates/Slicemaster.js
@@ -4,8 +4,27 @@ import Img from 'gatsby-image';
 import styled from 'styled-components';
 
 // how to destructure a property two levels deep in a parameter:
-export default function SlicemasterPage() {
+export default function SlicemasterPage({ data }) {
+  console.log(data);
   return (
     <p>nice</p>
   );
 }
+
+export const query = graphql`
+  query($slug: String!) {
+    person: sanityPerson(slug: { current: { eq: 
+    $slug } }) {
+      name
+      id
+      description
+      image {
+        asset {
+          fluid(maxWidth: 1000, maxHeight: 750) {
+            ...GatsbySanityImageFluid
+          }
+        }
+      }
+    }
+  }
+`;
